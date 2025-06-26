fd89d477f3409d2968724d63b61ef24eae4d21b8
Sahil <sahilpoply28@gmail.com>
2025-06-26 23:59:16 +0530

    Use applicationDefault() for Firebase Admin to support cloud deployment

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
   });
 }
 
